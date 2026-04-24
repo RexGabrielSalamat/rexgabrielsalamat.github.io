898654a4c4fc94dada6b3e8b9d0961af5538c20e
Rex Gabriel <gabbysalamat@gmail.com>
2026-04-24 20:39:02 +0400

    Update cert-script.js

--- cert-script.js
+++ cert-script.js
@@ -10,7 +10,7 @@ const trainingCerts = [
     { file: 'assets/certificates/Certificate of Completion - IoT Foundations - Fundamentals.jpg', title: 'IoT Foundations', org: 'LinkedIn Learning', tags: ['IoT', 'Hardware'] },
     { file: 'assets/certificates/Certificate of Completion - Programming Foundations - Inside Computing Hardware.jpg', title: 'Inside Computing Hardware', org: 'LinkedIn Learning', tags: ['Hardware', 'Architecture'] },
     { file: 'assets/certificates/Certificate of Completion - Software Design - Developing Effective Requirements.jpg', title: 'Software Design Requirements', org: 'LinkedIn Learning', tags: ['SDLC', 'Design'] },
-    { file: 'assets/certificates/Certificate of Completion - Introduction to Web Design and Development', title: 'Introduction to Web Design and Development', org: 'LinkedIn Learning', tags: ['Web Development', 'HTML', 'CSS', 'JS'] }
+    { file: 'assets/certificates/Certificate of Completion - Introduction to Web Design and Development.jpg', title: 'Introduction to Web Design and Development', org: 'LinkedIn Learning', tags: ['Web Development', 'HTML', 'CSS', 'JS'] }
 ];
 
 const eventCerts = [
